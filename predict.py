67409408ff8f1bc2caf3ea279a1f5018b306ab8c
Bt-PplusK <121921113+Bt-PplusK@users.noreply.github.com>
2023-11-06 19:50:55 +0630

    Update predict.py

--- predict.py
+++ predict.py
@@ -5,7 +5,7 @@ from flask import request
 from flask import jsonify
 
 
-model_file = 'model_C=1.0.bin'
+model_file = 'model_C=0.001.bin'
 
 with open(model_file, 'rb') as f_in:
     dv, model = pickle.load(f_in)
@@ -18,7 +18,7 @@ def predict():
 
     X = dv.transform([customer])
     y_pred = model.predict_proba(X)[0, 1]
-    churn = y_pred >= 0.5
+    heart_attack_risk = y_pred >= 0.5
 
     result = {
         'heart_attack_risk_probability': float(y_pred),
