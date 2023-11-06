acb1baaae6cea8f932c9328b9c1a3a2f814ca70b
Bt-PplusK <121921113+Bt-PplusK@users.noreply.github.com>
2023-11-06 14:04:14 +0630

    Create predict.py

---
+++ predict.py
@@ -0,0 +1,32 @@
+import pickle
+
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+
+model_file = 'model_C=1.0.bin'
+
+with open(model_file, 'rb') as f_in:
+    dv, model = pickle.load(f_in)
+
+app = Flask('heart_attack_risk')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    customer = request.get_json()
+
+    X = dv.transform([customer])
+    y_pred = model.predict_proba(X)[0, 1]
+    churn = y_pred >= 0.5
+
+    result = {
+        'heart_attack_risk_probability': float(y_pred),
+        'heart_attack_risk': bool(heart_attack_risk)
+    }
+
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
